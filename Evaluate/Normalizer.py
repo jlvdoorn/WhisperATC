4816a88ccd6c9ce4ca852fff22d3c682f380dbdb
Jan van Doorn <jlvdoorn@icloud.com>
2023-09-11 12:20:27 +0200

    Added some extensions to a local prompting scheme for the ANSP data

--- Evaluate/Normalizer.py
+++ Evaluate/Normalizer.py
@@ -141,6 +141,17 @@ def remove_multiples(string):
             
     return string
 
+def standard_words(text):    
+    text = text.lower()
+    
+    text = text.replace('lineup', 'line up')
+    text = text.replace('centre', 'center')
+    text = text.replace('k l m', 'klm')
+    text = text.replace('niner', 'nine')
+    text = text.replace('x-ray', 'xray')
+
+    return text
+
 def filterAndNormalize(text):   
     text = removeCharSet(text, '[', ']')
     text = removeCharSet(text, '<', '>')
@@ -157,7 +168,40 @@ def filterAndNormalize(text):
     text = splitGreetings(text)
     #text = remove_multiples(text)
     text = text.lower()
+    
+    # V2 Additions
+    text = standard_words(text)
     return text
 
 def normalizeOnly(text):
     return normalizer(text)
+
+# V2 Prompt 
+local_prompt_extension = [
+    # Waypoints
+    'ABNED', 'ABSAM', 'ADIKU', 'ADOMI', 'ADUNU', 'AGASO', 'AGISI', 'AGISU', 'AGOGO', 'AKOXA', 'AKZOM', 'ALFEN', 'ALINA', 'AMADA', 'AMEGA', 'AMGOD', 'AMREG', 'AMRIV', 'AMSOT', 'ANDIK', 'ANETS', 'ANZUL', 'APVUV', 'ARBEP', 'ARNEM', 'ARTIP', 'ARWIN', 'ASBES', 'ASGOS', 'ASNOM', 'ASTUW', 'ATRIX', 'ATWIT', 'BADEX', 'BAGOV', 'BAHSI', 'BAKLU', 'BANDU', 'BASGU', 'BASNO', 'BATAK', 'BAXIM', 'BEDUM', 'BEKEM', 'BEKVU', 'BELAP', 'BEMTI', 'BENUX', 'BERGI', 'BERIR', 'BESBU', 'BESTI', 'BETUS', 'BIBIS', 'BLUSY', 'BOBMO', 'BOGRU', 'BOGTI', 'BOVCO', 'BREDA', 'BRIAR', 'BUDIP', 'BUROG', 'DANUM', 'DENAG', 'DENUT', 'DERUV', 'DESUL', 'DEVIG', 'DEVUT', 'DEXOR', 'DIBIR', 'DIBRU', 'DIKAT', 'DIMOX', 'DINAK', 'DISRA', 'DIVPA', 'DOBAK', 'DOFMU', 'DOTIX', 'EBAGO', 'EBUSA', 'EDFOS', 'EDOXO', 'EDUBU', 'EDUMA', 'EDUPO', 'EHOJI', 'EKDAR', 'EKNON', 'EKROS', 'ELBED', 'ELPAT', 'ELSIK', 'ELSUR', 'EMMUN', 'ENKOS', 'EPOXU', 'ERMUR', 'ERSUL', 'ETEBO', 'ETPOS', 'EVELI', 'FAFLO', 'FEWEX', 'FLEVO', 'GALSO', 'GEMTI', 'GETSI', 'GIKOV', 'GILIV', 'GILTI', 'GIROS', 'GISEB', 'GOBNO', 'GODOS', 'GOHEM', 'GOLOR', 'GOTIG', 'GREFI', 'GRONY', 'GULTO', 'HAMZA', 'HECTI', 'HELEN', 'HELHO', 'HOXZA', 'IBALO', 'IBNOS', 'IDAKA', 'IDGOK', 'IDRID', 'IFTAZ', 'IMVUK', 'INBAM', 'INDEV', 'INDIX', 'INKET', 'INLOD', 'INRIP', 'INVIT', 'IPMUR', 'IPTAS', 'IPVIS', 'IRDUK', 'IVLUT', 'IVNUD', 'IXUTA', 'JOPFI', 'KAKKO', 'KAROF', 'KEGIT', 'KEKIX', 'KEROR', 'KOKIP', 'KOLAG', 'KOLAV', 'KONEP', 'KONOM', 'KOPAD', 'KOPFA', 'KUBAT', 'KUDAD', 'KUSON', 'KUVOS', 'LABIL', 'LAMSO', 'LANSU', 'LARAS', 'LARBO', 'LASEX', 'LEGBA', 'LEKKO', 'LEKSU', 'LERGO', 'LEVKI', 'LIKDO', 'LILSI', 'LOCFU', 'LONAM', 'LONLU', 'LOPIK', 'LUGUM', 'LUNIX', 'LUSOR', 'LUTET', 'LUTEX', 'LUTOM', 'LUVOR', 'MAPAD', 'MASOS', 'MAVAS', 'MAXUN', 'MEBOT', 'MIMVA', 'MITSA', 'MODRU', 'MOKUM', 'MOLIX', 'MOMIC', 'MONIL', 'NAKON', 'NAPRO', 'NARIX', 'NARSO', 'NAVAK', 'NAVPI', 'NEKAS', 'NELFE', 'NEPTU', 'NETEX', 'NETOM', 'NEWCO', 'NEXAR', 'NIDOP', 'NIGUG', 'NIHOF', 'NILMI', 'NIREX', 'NIRSI', 'NIXCO', 'NOFUD', 'NOGRO', 'NOLRU', 'NOPSU', 'NORKU', 'NOVEN', 'NOWIK', 'NYKER', 'OBAGU', 'OBILO', 'ODASI', 'ODVIL', 'OGBOL', 'OGINA', 'OKIDU', 'OKLOV', 'OKOKO', 'OLGAX', 'OLGER', 'OLWOF', 'OMASA', 'OMFAR', 'OMORU', 'ORCAV', 'OSGOS', 'OSKUR', 'OSPAV', 'OSRON', 'OSTIR', 'OTMEC', 'OTSEL', 'PAPOX', 'PELUB', 'PENIM', 'PEROR', 'PESER', 'PETCA', 'PETIK', 'PEVAD', 'PEVOS', 'PILEV', 'PIMIP', 'PINUS', 'PIPQU', 'PODOD', 'PORWA', 'PUFLA', 'PUTTY', 'RAKIX', 'RAVLO', 'REDFA', 'RELBI', 'RENDI', 'RENEQ', 'RENVU', 'REWIK', 'RIKOR', 'RIMBU', 'RINIS', 'RIVER', 'ROBIS', 'ROBVI', 'RODIR', 'ROFAC', 'ROLDU', 'ROMIN', 'RONSA', 'ROTEK', 'ROVEN', 'ROVOX', 'RUMER', 'RUSAL', 'SASKI', 'SETWO', 'SIDNI', 'SIPLO', 'SITSU', 'SOFED', 'SOGPO', 'SOKSI', 'SOMEL', 'SOMEM', 'SOMVA', 'SONEB', 'SONSA', 'SOPVI', 'SORAT', 'SOTAP', 'SUBEV', 'SUGOL', 'SULUT', 'SUMAS', 'SUMUM', 'SUPUR', 'SUSET', 'SUTEB', 'TACHA', 'TAFTU', 'TEBRO', 'TEMLU', 'TENLI', 'TEVKA', 'TIDVO', 'TILVU', 'TINIK', 'TIREP', 'TOLKO', 'TOPPA', 'TORGA', 'TORNU', 'TOTNA', 'TOTSA', 'TULIP', 'TUPAK', 'TUVOX', 'TUXAR', 'ULPAT', 'ULPEN', 'ULPOM', 'ULSED', 'UNATU', 'UNEXO', 'UNKAR', 'UNORA', 'UNVAR', 'UPLOS', 'UTIRA', 'UVOXI', 'VALAM', 'VALKO', 'VAPEX', 'VELED', 'VELNI', 'VENAV', 'VEROR', 'VEXAR', 'VICOT', 'VOLLA', 'WILEM', 'WINJA', 'WISPA', 'WOODY', 'XAMAN', 'XEBOT', 'XEKRI', 'XENEV', 'XIDES', 'XIPTA', 'XIPTI', 'XOMBI', 'XONLO', 'YENZO', 'YOGCE', 'YOJUP', 'ZITFA', 'ZOJIK',
+    
+    # Beacons (VOR/DME)
+    'Amsterdam', 'Den Helder', 'Eelde', 'Eindhoven', 'Haastrecht', 'Lelystad', 'Maastricht', 'Pampus', 'Rekken', 'Rotterdam', 'Schiphol', 'Spykerboor',
+    'AMS', 'HDR', 'EEL', 'EHV', 'FRT', 'FRO', 'MAS', 'PAM', 'RKN', 'RTM', 'SPL', 'SPY',
+    
+    # Places 
+    'Zandvoort',
+    
+    # Runways
+    'zero four', 'oh four', 'zero six', 'oh six', 'zero nine', 'oh nine', 
+    'two two', 'twenty two', 'two four', 'twenty four', 'two seven', 'twenty seven',
+    'one eight left', 'eighteen left', 'one eight right', 'eighteen right', 'one eight center', 'eighteen center',
+    'three six left', 'thirty six left', 'three six right', 'thirty six right', 'three six center', 'thirty six center',
+    
+    # Holding Points
+    'golf one', 'golf two', 'golf three', 'golf four', 'golf five',
+    'sierra one', 'sierra two', 'sierra three', 'sierra four', 'sierra five', 'sierra six', 'sierra seven',
+    'november one', 'november two', 'november three', 'november four', 'november five', 'november nine',
+    'echo one', 'echo two', 'echo three', 'echo four', 'echo five',
+    'whiskey one', 'whiskey two', 'whiskey three', 'whiskey four', 'whiskey five', 'whiskey six', 'whiskey seven',
+    'whiskey eight', 'whiskey nine', 'whiskey ten', 'whiskey eleven', 'whiskey twelve',
+    'victor one', 'victor two', 'victor three', 'victor four'
+]
+
+common_texts = ['line up and wait', 'line up runway', 'wind is calm', 'wake turbulence']
